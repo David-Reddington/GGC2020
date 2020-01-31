cc5b5e0a2a6251df467cf6d2e55504970d03a44e
Nicholas Poole <47387274+NPoole95@users.noreply.github.com>
2020-01-31 22:50:32 +0000

    Added in 2 seperate terrains
    one of the terrains is the paths to be used on the navmesh, the other is simply for the environment to look pretty

--- GGC2020/Assets/Scripts/Buildings/BuildingBase.cs
+++ GGC2020/Assets/Scripts/Buildings/BuildingBase.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshObstacle))]
 public class BuildingBase : MonoBehaviour
 {
     [Range(0, 1000)]
     public uint mOccupancy;
 
+    public GameObject visionLightPrefab;
+
     [SerializeField]
     string mTag;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        GameObject visionLight = Instantiate(visionLightPrefab, new Vector3(0, 0.5f, 0), Quaternion.Euler(90.0f, 0.0f, 0.0f)) as GameObject;
+        visionLight.transform.parent = transform;
     }
 
     // Update is called once per frame
