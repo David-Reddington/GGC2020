565c73e229311082baa5dc9dbf1cd19d3ab96219
aidenjones29 <36414445+aidenjones29@users.noreply.github.com>
2020-01-31 19:25:54 +0000

    Model prefab & cam edit
    ...

--- GGC2020/Assets/Scripts/Components/CameraComponent.cs
+++ GGC2020/Assets/Scripts/Components/CameraComponent.cs
@@ -7,9 +7,9 @@ public class CameraComponent : MonoBehaviour
 
     Transform mTransform;
 
-    float mainSpeed = 100.0f; //regular speed
-    float shiftAdd = 250.0f; //multiplied by how long shift is held.  Basically running
-    float maxShift = 1000.0f; //Maximum speed when holdin gshift
+    float mainSpeed = 50.0f; //regular speed
+    float shiftAdd = 100.0f; //multiplied by how long shift is held.  Basically running
+    float maxShift = 150.0f; //Maximum speed when holdin gshift
     float camSens = 0.25f; //How sensitive it with mouse
     private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
     private float totalRun = 1.0f;
@@ -30,16 +30,16 @@ public class CameraComponent : MonoBehaviour
         transform.rotation = mTransform.rotation;
         bIsAttached = false;
 
-        if (Input.GetMouseButton(1))
-        {
-            lastMouse = Input.mousePosition - lastMouse;
-            lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0);
-            lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
-            transform.eulerAngles = lastMouse;
-            //lastMouse = Input.mousePosition;
-            //Mouse  camera angle done. 
-        }
-        lastMouse = Input.mousePosition;
+        //if (Input.GetMouseButton(1))
+        //{
+        //    lastMouse = Input.mousePosition - lastMouse;
+        //    lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0);
+        //    lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
+        //    transform.eulerAngles = lastMouse;
+        //    //lastMouse = Input.mousePosition;
+        //    //Mouse  camera angle done. 
+        //}
+        //lastMouse = Input.mousePosition;
 
 
         //Keyboard commands
@@ -79,11 +79,11 @@ public class CameraComponent : MonoBehaviour
         Vector3 p_Velocity = new Vector3();
         if (Input.GetKey(KeyCode.W))
         {
-            p_Velocity += new Vector3(0, 0, 1);
+            p_Velocity += new Vector3(0, 1, 1);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            p_Velocity += new Vector3(0, 0, -1);
+            p_Velocity += new Vector3(0, -1, -1);
         }
         if (Input.GetKey(KeyCode.A))
         {
